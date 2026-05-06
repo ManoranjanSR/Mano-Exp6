31b8eb3feafd31cefdadfaac4433d554f50ee483
ManoranjanSR <1nc24ci067@ncetmail.com>
2026-05-06 10:11:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 def add(a, b):
-  return a + b
+    return a + b
 
-if __name__ == &quot;__main__&quot;: # pragma: no cover
-  result = add(2, 3)
-  print(result)
+if __name__ == "__main__": # pragma: no cover
+    result = add(2, 3)
+    print(result)
